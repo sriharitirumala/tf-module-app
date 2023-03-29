cac276cfd57ee15dbf244985646e13b742c6b45a
sriharitirumala <sritirumala30@gmail.com>
2023-03-29 14:42:30 +0530

    adding vpc subnets, routes, route table associations

--- vars.tf
+++ vars.tf
@@ -2,4 +2,7 @@ variable "component" {}
 variable "env" {}
 variable "instance_type" {}
 variable "tags" {}
-
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "subnets" {}
