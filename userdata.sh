fb001b25b4771ac3350cbb2ae9fa691ffde03ca2
sriharitirumala <sritirumala30@gmail.com>
2023-03-29 15:27:17 +0530

    adding vpc subnets, routes, route table associations

--- userdata.sh
+++ userdata.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-pull -i localhost, -U https://github.com/sriharitirumala/roboshop-ansible roboshop.yml -e role_name=${component} -e env=${env}
+ansible-pull -i localhost, -U https://github.com/sriharitirumala/roboshop-ansible roboshop.yml -e role_name=${component} -e env=${env} >/opt/ansible.log
