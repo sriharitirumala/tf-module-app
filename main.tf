9bfd2b66c2d28e867c30f882e2b5e84392e512db
sriharitirumala <sritirumala30@gmail.com>
2023-03-29 14:18:39 +0530

    adding vpc subnets, routes, route table associations

---
+++ main.tf
@@ -0,0 +1,28 @@
+resource "aws_launch_template" "main" {
+  name = "${var.component}-${var.env}"
+
+
+#  iam_instance_profile {
+#    name = "test"
+#  }
+
+  image_id = data.aws_ami.ami.id
+
+
+  instance_market_options {
+    market_type = "spot"
+  }
+
+  instance_type = var.instance_type
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = merge (
+      var.tags,
+      Name = "${var.component}-${var.env}"
+    )
+  }
+
+#  user_data = filebase64("${path.module}/example.sh")
+}
