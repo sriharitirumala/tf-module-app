923f262adec18d2f0e145afa407f94e797534139
sriharitirumala <sritirumala30@gmail.com>
2023-03-31 17:33:31 +0530

    adding vpc subnets, routes, route table associations

--- main.tf
+++ main.tf
@@ -112,3 +112,19 @@ resource "aws_route53_record" "main" {
   ttl     = 30
   records = [var.alb_dbs_name]
 }
+
+resource "aws_lb_listener_rule" "listener_rule" {
+  listener_arn = var.listener_arn
+  priority     = var.listener_priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+
+  condition {
+    host_header {
+      values = [local.dns_name]
+    }
+  }
+}
--- vars.tf
+++ vars.tf
@@ -12,5 +12,6 @@ variable "allow_app_to" {}
 variable "port" {}
 variable "dns_domain" {}
 variable "alb_dbs_name" {}
-
+variable "listener_arn" {}
+variable "listener_priority" {}
 
