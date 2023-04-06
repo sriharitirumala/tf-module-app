04b977fb1414698b8e80e24f5b3c74e1bd6e6266
sriharitirumala <sritirumala30@gmail.com>
2023-04-06 09:27:06 +0530

    adding vpc subnets, routes, route table associations

--- iam.tf
+++ iam.tf
@@ -16,10 +16,12 @@ resource "aws_iam_policy" "main" {
           "ssm:GetParameters",
           "ssm:GetParameter"
         ],
-        "Resource": [
-          "arn:aws:ssm:us-east-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.${var.component}.*",
-          "arn:aws:ssm:us-east-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.docdb.*"
-        ]
+#        "Resource": [
+#          "arn:aws:ssm:us-east-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.${var.component}.*",
+#          "arn:aws:ssm:us-east-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.docdb.*",
+#          "arn:aws:ssm:us-east-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.elasticache.*"
+#        ]
+        "Resource": [ for k in local.parameters : "arn:aws:ssm:us-east-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.${ k }.*"]
       },
       {
         "Sid": "VisualEditor1",
