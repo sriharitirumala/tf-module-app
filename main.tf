73a8f0bf245d6edb91fe49c0263953897344fbc4
sriharitirumala <sritirumala30@gmail.com>
2023-04-08 11:35:15 +0530

    adding vpc subnets, routes, route table associations

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_autoscaling_group" "main" {
   }
   tag{
     key = "name"
-    propagate_at_launch = false
+    propagate_at_launch = true
     value = "${var.component}-${var.env}"
 
   }
